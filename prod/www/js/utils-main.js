f7f5a90aa34901b62c07f1870a1a3b34d25a8230
Sergey Berengard <stuff@berense.org>
2025-12-24 06:34:28 +0200

    media plane testimonials

--- prod/www/js/utils-main.js
+++ prod/www/js/utils-main.js
@@ -468,6 +468,7 @@ function activateHelpful() {
 
 function carouselAutoScrollStart(slider, autoScrollInterval) {
 	if (autoScrollInterval > 0) clearInterval(autoScrollInterval);
+	console.log("carousel start");
 	return setInterval(() => {
 		console.log("scroll");
 		const cardWidth = slider.querySelector('.testimonial-card').offsetWidth + 24;
