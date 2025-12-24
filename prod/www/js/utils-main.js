5c32c8dc66f0a4432bec350274b3d29a190e520a
Sergey Berengard <stuff@berense.org>
2025-12-24 07:53:32 +0200

    better id scrolling

--- prod/www/js/utils-main.js
+++ prod/www/js/utils-main.js
@@ -70,6 +70,22 @@ function activateScroll() {
 			}
 		}
 	};
+
+	document.querySelectorAll('a[href^="#"]').forEach(link => {
+		link.addEventListener('click', function (e) {
+			const targetId = this.getAttribute('href');
+			const target = document.querySelector(targetId);
+
+			if (target) {
+				e.preventDefault();
+				target.scrollIntoView({
+					behavior: 'smooth',
+					block: 'center'
+				});
+				history.pushState(null, '', targetId);
+			}
+		});
+	});
 }
 
 function _setTOCSection(target) {
